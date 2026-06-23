cc0377bab6d06f16c7ff9f26731c84b4d07bd641
Emran Hossen <dev.emranhossen@gmail.com>
2026-06-23 13:02:36 +0600

    trying to fix registration issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,27 +3,41 @@ import { betterAuth } from 'better-auth'
 import { MongoClient } from 'mongodb'
 import dns from 'node:dns'
 
-dns.setServers(['1.1.1.1', '8.8.8.8'])
+// Force Node.js to use public DNS servers to properly resolve MongoDB SRV records
+console.log('[Auth] Setting DNS servers to 1.1.1.1 and 8.8.8.8');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+console.log('[Auth] Active DNS servers:', dns.getServers());
 
-const client = new MongoClient(process.env.MONGODB_URL || "")
+let client
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClient) {
+    global._mongoClient = new MongoClient(process.env.MONGODB_URL || "", {
+      connectTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 10
+    })
+  }
+  client = global._mongoClient
+} else {
+  client = new MongoClient(process.env.MONGODB_URL || "", {
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    maxPoolSize: 10
+  })
+}
+
 const db = client.db('SunCart_DB')
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db),
+  database: mongodbAdapter(db, {
+    client
+  }),
   
   secret: process.env.BETTER_AUTH_SECRET || "fallback_secret_for_validation_only",
 
-  databaseHooks: {
-    user: {
-      beforeCreate: async (user) => {
-        return {
-          data: {
-            ...user,
-            id: user.id || new Date().getTime().toString()
-          }
-        }
-      }
-    }
+  advanced: {
+    generateId: true
   },
 
   emailAndPassword: {
